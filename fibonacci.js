76b4c5ace2c945985c215d3f9b8fbf42b2f70807
Michael Pimentel <mtpjr88@gmail.com>
2017-08-06 22:11:00 -0700

    fibonacci algorithm

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+// fibonacci sequence
+
+function fibonacci(num){
+  var fibo = [0, 1];
+  
+  if (num <= 2) return 1;
+
+  for (var i = 2; i <=num; i++ ){
+   fibo[i] = fibo[i-1]+fibo[i-2];
+  }
+
+ return fibo[num];
+} 
+
+console.log(fibonacci(12));
