b92799a57ca0cae46db4a002b3c8de8322a73c9d
Michael Pimentel <mtpjr88@gmail.com>
2017-09-16 17:33:14 -0700

    comments to selection sort how it works

---
+++ selectionSort.js
@@ -0,0 +1,15 @@
+// *********** Selection Sort ************
+// How does it works:
+// Go through the array,find the index of the lowest element,
+// swap the lowest element with the first element.
+// Now first element is the lowest element in the array.
+// Now go through the rest of the array (excluding the first element) 
+// and find the index of the lowest and swap it with the second element.
+// thats how it continues to select (find out) the lowest element of the 
+// array and putting it on the left until it hits the last element.
+
+function selectionSort(arr) {
+    
+    
+    return arr;
+}
