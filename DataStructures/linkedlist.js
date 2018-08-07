12f8185ad92d152b94c93237e19960e450222a2d
Michael Pimentel <mtpjr88@gmail.com>
2018-08-07 09:40:26 -0700

    updated commits

--- DataStructures/linkedlist.js
+++ DataStructures/linkedlist.js
@@ -1,45 +1,45 @@
-	// Doubly Linked List
+class LinkedList {
+    constructor(head,tail){
+        this.head = head;
+        this.tail = tail;
+    }
 
-	// Constructor function or Class
-	function LinkedList () {
-		// set to null since when initially created
-		// they don't have any nodes
-		this.head = null;
-		this.tail = null;
-	}
+    addToHead(value) {
+        const newNode = new Node(value, this.head, null);
+// if there is a head node
+// make the newNode being created also its previous Node
+       if(this.head) {
+           this.head.prev = newNode;
+       } else {
+           // otherwise make the newNode also the the tail node since there is no other
+           this.tail = newNode;
+       }
+       // regardless make the new node also the head node
+        this.head = newNode;
+    }
 
-	// Each Node has three properties {value, next, and prev}
-	function Node (value, next, prev) {
-	// the value of the Node
-		this.value = value;
-	// A pointer referencing to the next Node (if present) otherwise null
-		this.next = next;
-	// A pointer referencing the previous Node, otherwise null
-		this.prev = prev;
-	}
+    addToTail(value) {
+        const newNode = new Node(value, null, this.tail);
+        if(this.tail) {
+            this.tail.next = newNode;
+        } else {
+            this.head = newNode;
+        }
+        this.tail = newNode;
+    }
+}
 
-	LinkedList.prototype.addToHead = function(value) {
-	// 1. Create a new Node (instance) that will be added to the `Head` of the LinkedList
-	// 2. this Method will take a `value` parameter that will be the value of the new Node.	
-		let newNode = new Node(value, this.head, null);
-	// 3. the `next` parameter passed will be 'this.head' refering to the LinkedList object `this.head`
-	// since we are adding a new Node, the Next in line would be the old head or initial `this.head`
-	// 4. the prev param will be null since this will be the new Head Node, therefore there is not a prev in line
-	// this.head is the Head
+class Node {
+    constructor(value, next, prev) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
 
-	// if there is a Node exists then set its prev pointer to reference a new Node 
-		if (this.head) this.head.prev = newNode;
-	// since this is the only Node, it will be both head Node and Tail
-		else this.tail = newNode;
-	// Now make newNode as the Head Node
-		this.head = newNode;
-	}
+const linkedList = new LinkedList();
+linkedList.addToHead(100);
+linkedList.addToHead(200);
+linkedList.addToTail(400);
 
-	let LL = new LinkedList();
-	LL.addToHead(100);
-	LL.addToHead(200);
-	console.log(LL);
-
-
-
-
+console.log(linkedList);
