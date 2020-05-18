07d75ccd5196280c6e59c9a26e13dbf1966a0b49
Michael Pimentel Jr <mtpjr88@Michaels-MacBook-Pro.local>
2020-05-18 11:01:05 -0700

    removed files, added binarySearch

--- DataStructures/BSD/bst.js
+++ DataStructures/BSD/bst.js
@@ -1,17 +1,31 @@
-function binarySearch (arr, itemSearch) {
-  let left = 0;
-  let right = arr.length-1;
-  let midPoint;
-    while(left <= right) {
-      midPoint = (left + right) / 2;
-      if(itemSearch < arr[midPoint]) right = midPoint-1;
-      else if(itemSearch > arr[midPoint]) left = midPoint+1;
-      else return midPoint;
-    }
-  return -1;
+/** Returns items index*/
+function binarySearch(target, arr) {
+	let start = 0;
+	let end = arr.length - 1;
 
+	while (start <= end) {
+		let mid = Math.floor((start + end) / 2);
+		if (arr[mid] === target) {
+			return mid;
+		} else if (target < arr[mid]) {
+			end = mid - 1;
+		} else {
+			start = mid + 1;
+		}
+	}
+	return -1;
 }
+/*
+                                  [1,2,3,4,5,6,7,8,9,10]
+                                   ^             ^            ^
+                                start        mid        end
+ 
+                  if target < mid point ( if target were 2 )
+                    move end to mid - 1 and viseversa
 
-console.log(binarySearch([1,2,3,4,5,6,7], 2));
+                        [1,    2,     3,    4,    5]
+                        ^             ^            ^
+                      start        mid        end
 
-//[][][][][][][][]][][][][]
+ */
+console.log(binarySearch(11, [3, 4, 5, 6, 7, 8, 9, 10]));
